7a376a8d60d4b1adac1a13614995c4f3d8455764
GeekyHacks <geekyhacks22@gmail.com>
2023-03-05 18:01:06 +0300

    push

--- JavaScriptNotes/javaScript.js
+++ JavaScriptNotes/javaScript.js
@@ -1,3 +1,12 @@
+
+
+//! Check the console as you code to see how it changes. 
+
+/*In this example, subarray has the value [[10, 11, 12], 13, 14],
+ nestedSubarray has the value [10, 11, 12], and element has the value 11 .
+Note: There shouldn't be any spaces between the array name and the square brackets,
+like array [0][0] and even this array [0] [0] is not allowed.  */
+
 const arr = [
     [1, 2, 3],
     [4, 5, 6],
@@ -12,4 +21,14 @@ const arr = [
   const element = arr[3][0][1];
   console.log(element);
 
-  
+  /*.pop() removes the last element from an array and returns that element.
+.push() adds the last element to an array and returns that element.
+ .shift() comes in. It works just like .pop(), except it removes the first
+  element instead of the last.
+  unshift() adds the element at the beginning of the array.
+ */
+
+const myArray = [["John", 23], ["cat", 2]];
+const removedFromMyArray= myArray.pop();
+const ourArray = ["Stimpson", "J", ["cat"]];
+const removedFromOurArray = ourArray.shift();
